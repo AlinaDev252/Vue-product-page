9f002fbd5ec74f2a2eb5455f9eca76e01bf41348
AlinaDev252 <costachealina25@gmail.com>
2021-05-06 15:30:49 +0300

    Add computed properties for our vue instance

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var app = new Vue({
 	el: "#app",
 	data: {
 		product: "Mugs",
-		image: "./assets/turquoise-mug.jpg",
+		brand: "Vue Mastery",
+		selectedVariant: 0,
 		link: "https://www.amazon.com/s?k=coffee+mugs&ref=nb_sb_noss_2",
-		inStock: false,
 		onSale: true,
 		details: ["clay mineral", "quartz", "silica"],
 		sizes: ["small", "medium", "large", "extra-large"],
@@ -13,16 +13,19 @@ var app = new Vue({
 				variantId: 2234,
 				variantColor: "turquoise",
 				variantImage: "./assets/turquoise-mug.jpg",
+				variantQuantity: 10,
 			},
 			{
 				variantId: 2235,
 				variantColor: "pink",
 				variantImage: "./assets/pink-mug.jpg",
+				variantQuantity: 0,
 			},
 			{
 				variantId: 2236,
 				variantColor: "yellow",
 				variantImage: "./assets/yellow-mug.jpg",
+				variantQuantity: 5,
 			},
 		],
 		cart: 0,
@@ -34,8 +37,20 @@ var app = new Vue({
 		// removeFromCart() {
 		// 	this.cart -= 1;
 		// },
-		updateProduct(variantImage) {
-			this.image = variantImage;
+		updateProduct(index) {
+			this.selectedVariant = index;
+			console.log(index);
+		},
+	},
+	computed: {
+		title() {
+			return this.brand + " " + this.product;
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity;
 		},
 	},
 });
